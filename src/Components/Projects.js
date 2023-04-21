e2f9ab65dc462c050044a84585a4fdd64c330876
samjaberi <samanjaberi.sj@gmail.com>
2023-04-20 20:15:26 -0400

    added expan expanding animation to project divs

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,6 +6,9 @@ import cover3 from '../assets/thegame.jpeg'
 import vid1 from '../assets/e3.mp4'
 import vid2 from '../assets/Acrolix.mp4'
 import vid3 from '../assets/Game.mp4'
+import bwCover1 from '../assets/euphonia2.JPG'
+import bwCover2 from '../assets/typewriter2.JPG'
+import bwCover3 from '../assets/thegame2.JPG'
 export default function Projects({ campaignMode, setCampaignMode }) {
     const [projects, setProjects] = useState([])
     const [focus, setFocus] = useState([])
@@ -13,13 +16,14 @@ export default function Projects({ campaignMode, setCampaignMode }) {
     const xRef = useRef(0)
 
     class Project{
-        constructor(title, description, liveLink, github, vidSrc, coverSrc){
+        constructor(title, description, liveLink, github, vidSrc, coverSrc, bwCover){
             this.title = title
             this.description = description
             this.liveLink = liveLink
             this.github = github
             this.vidSrc = vidSrc
             this.coverSrc = coverSrc
+            this.bwCover = bwCover
         }
     }
     useEffect(() => {
@@ -41,15 +45,13 @@ export default function Projects({ campaignMode, setCampaignMode }) {
         ]
         const videoSrcs = [vid1, vid2, vid3]
         const coverSrcs = [cover1, cover2, cover3]
+        const bwCovers = [bwCover1, bwCover2, bwCover3]
         setProjects(
             titles.map((title, i) => {
-                let project = new Project(title, descriptions[i], liveLinks[i], githubs[i], videoSrcs[i], coverSrcs[i])
+                let project = new Project(title, descriptions[i], liveLinks[i], githubs[i], videoSrcs[i], coverSrcs[i], bwCovers[i])
                 return project
             })
         )
-        console.log('campaign mode on launch', campaignMode)
-
-
     },[])
     const handleFocus = (project) => {
         const order = [...projects]
@@ -125,7 +127,25 @@ export default function Projects({ campaignMode, setCampaignMode }) {
         setCampaignMode(false)
         setFocus([])
     }, [x])
-    
+
+    const expand = (project) => {
+        setTimeout(() => {
+            const container = document.querySelectorAll('.projectContainer')[projects.indexOf(project)]
+            container.animate({
+                width:'80vw',
+                height:'400px'
+            },{duration: 500, fill:'forwards'})
+        }, 500)
+    }
+    const minimize = (project) => {
+        setTimeout(() => {
+            const container = document.querySelectorAll('.projectContainer')[projects.indexOf(project)]
+            container.animate({
+                width:'200px',
+                height:'400px'
+            },{duration: 400, fill:'forwards'})
+        }, 500)
+    }
     return(
         <>
             {   focus.length !== 0 ?
@@ -146,12 +166,16 @@ export default function Projects({ campaignMode, setCampaignMode }) {
                         <div id='projectTrack' data-mouse-down-at='0' data-prev-percentage='0' data-prev-scroll='0' data-percent-scroll='0'>
                             { projects.map(project => {
                                 return (
-                                    <div className='projectContainer'>
+                                    <div 
+                                    className='projectContainer' 
+                                    onMouseEnter={() => expand(project)}
+                                    onMouseLeave={() => minimize(project)}
+                                    >
                                         <div className='img-box' onClick={() => {
                                             handleFocus(project)
                                             }}>
                                             {/* <div className='overlay'></div> */}
-                                            <img src={project.coverSrc} className='image' draggable='false'></img>
+                                            <img src={project.bwCover} className='image' draggable='false'></img>
                                         </div>
                                     </div>
                                 )
