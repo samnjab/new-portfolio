8f242218ed597161b600e42c5a0117e01ce71deb
samjaberi <samanjaberi.sj@gmail.com>
2023-04-24 04:14:15 -0400

    project titles

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -15,10 +15,11 @@ export default function Projects({ campaignMode, setCampaignMode }) {
     const [x, setX] = useState(0)
     const xRef = useRef(0)
     const [fullview,setFullView] = useState()
-
+    const [trackPosition, setTrackPosition] = useState(0)
+    const trackXRef = useRef(0)
 
     class Project{
-        constructor(title, description, liveLink, github, vidSrc, coverSrc, bwCover){
+        constructor(title, description, liveLink, github, vidSrc, coverSrc, bwCover, titleColors){
             this.title = title
             this.description = description
             this.liveLink = liveLink
@@ -26,6 +27,7 @@ export default function Projects({ campaignMode, setCampaignMode }) {
             this.vidSrc = vidSrc
             this.coverSrc = coverSrc
             this.bwCover = bwCover
+            this.titleColors = titleColors
         }
     }
     useEffect(() => {
@@ -48,9 +50,10 @@ export default function Projects({ campaignMode, setCampaignMode }) {
         const videoSrcs = [vid1, vid2, vid3]
         const coverSrcs = [cover1, cover2, cover3]
         const bwCovers = [bwCover1, bwCover2, bwCover3]
+        const titleColors = [['#e0afa0', '#f4f3ee', '#bcb8b1', '#8a817c', '#463f3a'], ['#177e89', '#efe6dd', '#f3dfa2', '#bb4430', '#231f20'], ['#01497c', '#01497c', '#014f86', '#2a6f97', '#468faf', '#61a5c2', '#89c2d9', '#a9d6e5']]
         setProjects(
             titles.map((title, i) => {
-                let project = new Project(title, descriptions[i], liveLinks[i], githubs[i], videoSrcs[i], coverSrcs[i], bwCovers[i])
+                let project = new Project(title, descriptions[i], liveLinks[i], githubs[i], videoSrcs[i], coverSrcs[i], bwCovers[i], titleColors[i])
                 return project
             })
         )
@@ -64,7 +67,6 @@ export default function Projects({ campaignMode, setCampaignMode }) {
     }
     useEffect(() => {
         if (focus.length === 0) return
-        console.log('setting focus') 
         setCampaignMode(true)
     }, [focus])
     // Track Scroll Logic
@@ -99,7 +101,7 @@ export default function Projects({ campaignMode, setCampaignMode }) {
         const section = document.querySelector('.projects')
         section.onscroll = (e) => {
             const scrollDelta = parseFloat(track.dataset.prevScroll) - e.target.scrollLeft
-            const maxScrollDelta = window.innerWidth / 2 
+            const maxScrollDelta = track.offsetWidth / 6 
             const percentage = 2 * (scrollDelta / maxScrollDelta) * 100
             const nextPercentageUnconstrained = parseFloat(track.dataset.percentScroll) + percentage
             const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100)
@@ -129,20 +131,34 @@ export default function Projects({ campaignMode, setCampaignMode }) {
         setCampaignMode(false)
         setFocus([])
     }, [x])
+    useEffect(() => {
+        if (campaignMode || !fullview) return
+        const projectTrack = document.querySelector('.projects')
+        projectTrack.scrollLeft += (projectTrack.scrollWidth - document.body.clientWidth) / 2
+        console.log('track scroll left', projectTrack.scrollLeft )
+        trackXRef.current = projectTrack.scrollLeft
+        projectTrack.onscroll = () => {
+            setTrackPosition(projectTrack.scrollLeft)
+        }
+    }, [fullview])
+    useEffect(() => {
+        if (Math.abs(trackPosition - trackXRef.current) < 5 ) return
+        setFullView()
+    }, [trackPosition])
 
     const expand = (project) => {
         const container = document.querySelectorAll('.projectContainer')[projects.indexOf(project)]
-        container.animate({
-            width:'80vw',
+        // container.animate({
+        //     width:'80vw',
             
-        },{duration: 500, fill:'forwards'})
+        // },{duration: 500, fill:'forwards'})
         setFullView(project)
     }
     const minimize = (project) => {
         const container = document.querySelectorAll('.projectContainer')[projects.indexOf(project)]
-        container.animate({
-            width:'200px',
-        },{duration: 400, fill:'forwards'})
+        // container.animate({
+        //     width:'200px',
+        // },{duration: 400, fill:'forwards'})
         setFullView()
     }
     return(
@@ -166,19 +182,41 @@ export default function Projects({ campaignMode, setCampaignMode }) {
                             { projects.map(project => {
                                 return (
                                     <div 
-                                    className='projectContainer' 
-                                    onMouseOverCapture={() => expand(project)}
-                                    onMouseOutCapture={() => minimize(project)}
+                                    className={`projectContainer ${fullview === project ? 'rgbCover' : 'bwCover'}`} 
                                     >
-                                        <div className='img-box' onClick={() => {
-                                            handleFocus(project)
-                                            }}>
+                                        <div className='projectTitle'>
                                             {
-                                                project === fullview ?
-                                                <img src={project.coverSrc}></img>
-                                                :
-                                                <img src={project.bwCover} className='image' draggable='false'></img>
+                                                [...project.title].map(letter => {
+                                                    return(
+                                                        <h3 style = {{color:project.titleColors[Math.floor(Math.random() * project.titleColors.length)]}} className={`${fullview === project ? '' : 'hide'}`}>{letter}</h3>
+                                                    )
+                                                })
                                             }
+                                        </div>
+                                        <div className='img-box'>
+                                                <img 
+                                                src={project.coverSrc} 
+                                                className={`image ${fullview === project ? '' : 'hide'}`} 
+                                                draggable='false'
+                                                onClick={() => {
+                                                handleFocus(project)
+                                                }}
+                                                ></img>
+                                                <div 
+                                                className={`exploreTag ${fullview === project ? '' : 'hide'}`}
+                                                onClick={() => {
+                                                handleFocus(project)
+                                                }}
+                                                >
+                                                    <p>Explore</p>
+                                                    <p>+</p>
+                                                </div>
+                                                <img 
+                                                src={project.bwCover} 
+                                                className={`image ${fullview !== project ? '' : 'hide'}`} 
+                                                draggable='false'
+                                                onClick={() => expand(project)}
+                                                ></img>
                                         </div>
                                     </div>
                                 )
