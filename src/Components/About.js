75277be9365ec73e1dec9766d5e3779721970177
samjaberi <samanjaberi.sj@gmail.com>
2023-06-20 18:48:57 -0400

    implementing smooth scroller on about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,9 +1,28 @@
+import { useState, useEffect, useRef } from'react'
 export default function About({mode, setMode}) {
+    const scrollBy = (e) => {
+        const target = e.target
+        const scrollDelta = parseFloat(target.dataset.prevScroll) - target.scrollTop
+        const maxDelta = e.target.scrollTopMax / 2
+        console.log('scrolldelta', scrollDelta, 'scrollMax', maxDelta)
+        const percentage = (scrollDelta / maxDelta) * -100
+        console.log('delta percent', percentage)
+        const nextPercentageUnconstrained = parseFloat(e.target.dataset.prevScrollPercent) + percentage
+        console.log('unconstrained', nextPercentageUnconstrained)
+        const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100)
+        target.dataset.prevScrollPercent = nextPercentage
+        target.dataset.prevScroll = target.scrollTop
+        target.animate({
+            transform: `translate3d(0%, ${nextPercentage}%, 0%)`
+        }, { duration: 1200, fill: "forwards" })
+    }
+
     return(
         <div className='about'>
             <div className='body'>
                 <div className='wrapper'>
                     <div className='shadow aboutSection'>
+                        <div className='scroller'></div>
                         <div className='intro'>
                             <p>Sam is an Interactive Digital Creater based in Toronto. She specializes in creating visual experiences with a strong focus on interaction. </p> 
                             <p> Sam is of Persian and Turkish descent, and grew up in a small town near Tehran where she excelled at mathematics and computer science. She moved to Toronto at 17 to attend the University of Toronto where she majored in Neuroscience.</p>
@@ -34,36 +53,42 @@ export default function About({mode, setMode}) {
                             </fieldset>
                         </div>
                     </div>
-                    <div className='aboutMainSection aboutSection'>
-                        <div className='intro'>
-                            <p>Sam is an Interactive Digital Creater based in Toronto. She specializes in creating visual experiences with a strong focus on interaction. </p> 
-                            <p> Sam is of Persian and Turkish descent, and grew up in a small town near Tehran where she excelled at mathematics and computer science. She moved to Toronto at 17 to attend the University of Toronto where she majored in Neuroscience.</p>
-                            <p>Aside from being a tech creator, Sam is a music fanatic, avid boxer and reader.</p>
-                            <p>Few things in life however, have topped her number one obsession: turning complex problems into elegant solutions.</p>
-                            <p>Sam is a graduate of Juno College of Technology (formerly HackerYou) Web Development Bootcamp. A unique combination of technical skills and a deep understanding of the human psychology drives her endless quest to uncover what makes us tick. </p> 
-                        </div>
-                        <div className='skills'>
-                            <h3>Tech Stack</h3>
-                            <p>Javascript & Interactive Design</p>
-                            <p>React</p>
-                            <p>NodeJs</p>
-                            <p>Python</p>
-                        </div>
-                        <div className='contact'>
-                            <h3>Get In Touch</h3>
-                            <p class="intro-text">Ready to work together on a design project? Want me on your team, or just want to say hello, how are ya? Fill out the form.</p>
-                            <fieldset>
-                            <form action="submit">
-                                <label htmlFor="name"class="sr-only">Your Name</label>
-                                <input type="text" id="name" name="name" placeholder="Your Name" />
-                                <label htmlFor="email" className="sr-only">Your Email</label>
-                                <input type="text" id="email" placeholder="Your Email" name="email" />
-                                <label htmlFor="message" className="sr-only">Your Message</label>
-                                <textarea type="text" id="message" placeholder="Your Message" name="message" rows="8"></textarea>
-                                <button>Inquire</button>
-                            </form>
-                            </fieldset>
+                    <div className='scrollable'
+                    onScroll= {(e) => scrollBy(e)}
+                    data-prev-scroll='0'
+                    data-prev-scroll-percent='0'
+                    >
+                        <div className='aboutMainSection aboutSection'>
+                            <div className='intro'>
+                                <p>Sam is an Interactive Digital Creater based in Toronto. She specializes in creating visual experiences with a strong focus on interaction. </p> 
+                                <p> Sam is of Persian and Turkish descent, and grew up in a small town near Tehran where she excelled at mathematics and computer science. She moved to Toronto at 17 to attend the University of Toronto where she majored in Neuroscience.</p>
+                                <p>Aside from being a tech creator, Sam is a music fanatic, avid boxer and reader.</p>
+                                <p>Few things in life however, have topped her number one obsession: turning complex problems into elegant solutions.</p>
+                                <p>Sam is a graduate of Juno College of Technology (formerly HackerYou) Web Development Bootcamp. A unique combination of technical skills and a deep understanding of the human psychology drives her endless quest to uncover what makes us tick. </p> 
+                            </div>
+                            <div className='skills'>
+                                <h3>Tech Stack</h3>
+                                <p>Javascript & Interactive Design</p>
+                                <p>React</p>
+                                <p>NodeJs</p>
+                                <p>Python</p>
+                            </div>
+                            <div className='contact'>
+                                <h3>Get In Touch</h3>
+                                <p class="intro-text">Ready to work together on a design project? Want me on your team, or just want to say hello, how are ya? Fill out the form.</p>
+                                <fieldset>
+                                <form action="submit">
+                                    <label htmlFor="name"class="sr-only">Your Name</label>
+                                    <input type="text" id="name" name="name" placeholder="Your Name" />
+                                    <label htmlFor="email" className="sr-only">Your Email</label>
+                                    <input type="text" id="email" placeholder="Your Email" name="email" />
+                                    <label htmlFor="message" className="sr-only">Your Message</label>
+                                    <textarea type="text" id="message" placeholder="Your Message" name="message" rows="8"></textarea>
+                                    <button>Inquire</button>
+                                </form>
+                                </fieldset>
 
+                            </div>
                         </div>
                     </div>
                 </div>
